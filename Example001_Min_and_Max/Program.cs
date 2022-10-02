c92cd4701ee4afc9ef11b7106913146e2bb33bd1
boris.kondrashov <suborg@mail.ru>
2022-10-02 20:09:28 +0500

    done 1 example

--- Example001_Min_and_Max/Program.cs
+++ Example001_Min_and_Max/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Из двух чисел a и b можно определить большее и меньшее");
+Console.Write("Введите число a= ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b= ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max "+a+" min "+b);
+}
+    else 
+{
+    Console.Write("max "+b+" min "+a);
+}  
