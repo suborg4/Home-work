c9084452a8da7b1014f0914629da7abda505c5bb
boris.kondrashov <suborg@mail.ru>
2022-10-02 20:42:28 +0500

    done 1 example final

--- Example001_Min_and_Max/Program.cs
+++ Example001_Min_and_Max/Program.cs
@@ -5,16 +5,15 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Из двух чисел a и b можно определить большее и меньшее");
-Console.Write("Введите число a= ");
+Console.Write("Введите число a = ");
 int a = int.Parse(Console.ReadLine());
-Console.Write("Введите число b= ");
+Console.Write("Введите число b = ");
 int b = int.Parse(Console.ReadLine());
 if (a > b)
 {
-    Console.Write("max "+a+" min "+b);
+    Console.Write("max = "+a+" min = "+b);
 }
     else 
 {
-    Console.Write("max "+b+" min "+a);
+    Console.Write("max = "+b+" min = "+a);
 }  
